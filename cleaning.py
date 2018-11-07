53db346c6170057094c5f9377560934942fd2431
mzaidi59 <mohdabbaszaidi@MOHDs-MacBook-Air.local>
2018-11-07 15:11:06 +0530

    list2dataset added

--- cleaning.py
+++ cleaning.py
@@ -28,5 +28,7 @@ if __name__ == '__main__':
     # file = open(filename, 'r')
     # text = file.read()
     # file.close()
-    text = "some text"
-    data = main(text)
+    text_list = ["some text. However it is missing", "I am happy today. I will go to camp"]
+    for text in text_list:
+        data = main(text)
+        print(data)
