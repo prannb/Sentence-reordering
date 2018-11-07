fd1dedccd600038f5acffa18d39c1054adbf817e
Tushar Gupta <tushargupta5197@gmail.com>
2018-11-07 16:42:16 +0000

    made list of lists for dataset

--- dataset2list.py
+++ dataset2list.py
@@ -1,14 +1,15 @@
 import csv
-
-
+import pickle
 def create_list(filename = 'data/training_set_rel3.tsv'):
 	with open(filename, 'r') as tsv:
+		print(tsv)
 		a = [line.strip().split('\t') for line in tsv]
 
 	dataset = []
 
-	for ele in a:
-		dataset.append(ele[2][1:-1])
+	# for ele in a:
+	# 	dataset.append(ele[2][1:-1])
 	return dataset
-
-# print statements
+filename = "train_list"
+file_obj = open(filename, 'wb')
+pickle.dump(create_list(), file_obj)
