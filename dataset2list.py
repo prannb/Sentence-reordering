35ba815cc613102c2af30fc1b4d4061e454ea998
Tushar Gupta <tushargupta5197@gmail.com>
2018-11-10 16:03:40 +0000

    new data safe for python3: python3_data.tsv

--- dataset2list.py
+++ dataset2list.py
@@ -1,15 +1,17 @@
 import csv
 import pickle
-def create_list(filename = 'data/training_set_rel3.tsv'):
+def create_list(filename = 'data/python3_data.tsv'):
 	with open(filename, 'r') as tsv:
 		print(tsv)
 		a = [line.strip().split('\t') for line in tsv]
 
 	dataset = []
 
-	# for ele in a:
-	# 	dataset.append(ele[2][1:-1])
+	for ele in a:
+		dataset.append(ele[2][1:-1])
 	return dataset
 filename = "train_list"
 file_obj = open(filename, 'wb')
-pickle.dump(create_list(), file_obj)
+data = create_list()
+# pickle.dump(data, file_obj)
+# print(data)
