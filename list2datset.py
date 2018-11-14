b96d2a3f0932755afa1db3548dd961af5e941fa3
mzaidi59 <mohdabbaszaidi@MOHDs-MacBook-Air.local>
2018-11-14 21:19:00 +0530

    Doc2Vec

--- list2datset.py
+++ list2datset.py
@@ -3,16 +3,14 @@ import random as ran
 import pickle
 import gensim
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-
-from keras.models import Sequential
-from keras.layers import LSTM,Dense, Activation, Lambda, TimeDistributed
-from sklearn.model_selection import train_test_split
-
 from cleaning_whole import get_dataset
 #--------------------Model Variables-----------
 sen_vec_len = 300
 max_sent = 10
 NUM_CLASS = 10
+
+embedding = 'doc2vec'
+# embedding = 'word2vec'
 
 # input_lists = get_dataset()
 # pkl_filename = 'input_lists.pkl'
@@ -69,10 +67,10 @@ with open(pkl_filename, 'rb') as file:
 #           ['rather', 'child', 'computer', 'learning', 'chatting', 'playing', 'games', 'safe', 'sound', 'home', 'community', 'place'], 
 #           ['hope', 'reached', 'point', 'understand', 'agree', 'computers', 'great', 'effects', 'child', 'gives', 'us', 'time', 'chat', 'friendsnew', 'people', 'helps', 'us', 'learn', 'globe', 'believe', 'keeps', 'us', 'troble'], 
 #           ['thank', 'listening']]]
-
-print("Loading....")
-wmodel = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True) 
-print("loaded")
+if embedding == 'word2vec':
+  print("Loading Word2vec Model....")
+  wmodel = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True) 
+  print("Loaded")
 
 def infer_avg_word_vec(sen_words, vec_size = 300):
   ndoc = len(sen_words)
@@ -87,7 +85,7 @@ def infer_avg_word_vec(sen_words, vec_size = 300):
   wtv = wtv/co
   return wtv
 
-def list2mat(input_lists = input_lists, vec_size = 300):
+def list2mat_word_vec(input_lists = input_lists, vec_size = 300):
   #returns sentence matrix numb_sent x vec_size, randomly shuffled matrix, shuffling order
 
   n_sent = len(input_lists)
@@ -95,14 +93,22 @@ def list2mat(input_lists = input_lists, vec_size = 300):
   shuff_sen_mat = np.zeros((n_sent, vec_size))
   shuff_order = list(range(n_sent))
   ran.shuffle(shuff_order)
-  # documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(input_lists)]
-  # model = Doc2Vec(documents, vector_size=vec_size, window=2, min_count=1, workers=4)
-  # for i,sent in enumerate(input_lists):
-  #   sen_mat[i,:] = model.infer_vector(input_lists[i])
   for i,sent in enumerate(input_lists):
       sen_mat[i,:] = infer_avg_word_vec(input_lists[i])
+  # print(sen_mat.shape)
+  for i in range(n_sent):
+    shuff_sen_mat[i,:] = sen_mat[shuff_order[i]]
+  return sen_mat, shuff_sen_mat, shuff_order
 
-
+def list2mat_doc_vec(doc2vec_model, input_lists = input_lists, vec_size = 300):
+  #returns sentence matrix numb_sent x vec_size, randomly shuffled matrix, shuffling order
+  n_sent = len(input_lists)
+  sen_mat = np.zeros((n_sent, vec_size))
+  shuff_sen_mat = np.zeros((n_sent, vec_size))
+  shuff_order = list(range(n_sent))
+  ran.shuffle(shuff_order)
+  for i,sent in enumerate(input_lists):
+    sen_mat[i,:] = doc2vec_model.infer_vector(input_lists[i])
   # print(sen_mat.shape)
   for i in range(n_sent):
     shuff_sen_mat[i,:] = sen_mat[shuff_order[i]]
@@ -133,9 +139,22 @@ def format_data(input_lists = input_lists):
   Y = np.zeros((n,max_sent,max_sent))
   order = np.zeros((n,max_sent))
   order = order-1
+  if embedding == 'doc2vec':
+    sentences = []
+    # for point in input_lists:
+    #   for sent in point:
+    #     sentences.append(sent)
+    # documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(sentences)]
+    # doc2vec_model = Doc2Vec(documents, vector_size=vec_size, window=2, min_count=1, workers=4)  
+    with open('models/doc2vec_model.pkl','rb') as file:
+      doc2vec_model = pickle.load(file)
   for i in range(len(input_lists)):
     print(i)
-    a,b,c = list2mat(input_lists[i])
+    if embedding == 'doc2vec':
+      a,b,c = list2mat_doc_vec(doc2vec_model, input_lists[i])
+    elif embedding == 'word2vec':
+      print("Word2Vec Embedding")
+      a,b,c = list2mat_word_vec(input_lists[i])
     order[i,0:len(c)] = c
     n_sen_mat[i,0:a.shape[0],0:a.shape[1]] = a
     n_shuff_sen_mat[i,0:b.shape[0],0:b.shape[1]] = b
